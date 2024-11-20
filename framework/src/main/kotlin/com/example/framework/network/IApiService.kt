09f44c97796960df931b46ecadbd6c70e31805c4
judithMarg <judithmargaritapacollalli@gmail.com>
2024-11-20 02:02:10 -0400

    fix problems

--- framework/src/main/kotlin/com/example/framework/network/IApiService.kt
+++ framework/src/main/kotlin/com/example/framework/network/IApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface IApiService {
-    @GET("/3/discover/movie?sort_by=popularity.desc&api_key=fa3e844ce31744388e07fa47c7c5d8c3")
+    @GET("/3/discover/movie?page=3&api_key=5740e9fe49b8252e514c6daa48d6463a&language=es")
     suspend fun fetchData(): Response<NetworkResponse>
 }
